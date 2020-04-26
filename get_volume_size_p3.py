0ce6cb245e97eae6f6f444600d0dada410b3284e
hlc-369123 <31560195+hlc-369123@users.noreply.github.com>
2020-04-26 22:40:16 +0800

    Create get_volume_size_p3.py

---
+++ get_volume_size_p3.py
@@ -0,0 +1,25 @@
+#! /usr/bin/env python3
+#__author__ : Hlc
+#__date__   : 2020/4/26
+
+import os,sys,json
+os.system("ceph osd pool ls")
+pool_name = input('pool_name>>>: ').strip()
+if_pool = os.popen("ceph osd pool ls|grep %s" % pool_name).read().strip()
+if pool_name != if_pool:
+    print("cat't find %s pool" % pool_name)
+    sys.exit(0)
+
+volume_list = os.popen("rbd ls %s" % pool_name).read().strip()
+volume_file = open("/tmp/volume_list","w",encoding="utf-8")
+volume_file.write(volume_list)
+volume_file.close()
+
+volume_file = open("/tmp/volume_list","r",encoding="utf-8")
+volume_size_to = 0
+for volume in volume_file.readlines() :
+    volume_info = json.loads(os.popen("rbd info %s/%s --format json" %(pool_name,volume.strip())).read())
+    volume_size = volume_info["size"]
+    volume_size_to += volume_size
+volume_size_to = volume_size_to/1024/1024/1024
+print("pool_name: %s,volume_size: %s GB" % (pool_name,volume_size_to))

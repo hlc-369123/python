ed0501e69d2e4f338a9ebbe725b5783e87c2a56b
hlc-369123 <31560195+hlc-369123@users.noreply.github.com>
2020-09-22 09:49:14 +0800

    Update Headbucket.py

--- aws/Headbucket.py
+++ aws/Headbucket.py
@@ -4,22 +4,29 @@
 # __date__   : 2020/9/20
 # head_bucket
 
-import boto3,json
+import boto3, json
+import logging
+from botocore.exceptions import ClientError
 
 
 class CONNECTION(object):
     def __init__(self, url=None):
         self.client = boto3.client('s3', endpoint_url=url)
+
     def head_object(self, bucket_name=None):
-        response = self.client.head_bucket(
-            Bucket=bucket_name,
-            # ExpectedBucketOwner='string'
-        )
-        print(json.dumps(response, sort_keys=True, indent=4, separators=(',', ':')))
+        try:
+            response = self.client.head_bucket(
+                Bucket=bucket_name,
+                # ExpectedBucketOwner='string'
+            )
+            print(json.dumps(response, sort_keys=True, indent=4, separators=(',', ':')))
+        except ClientError as e:
+            logging.error(e)
+            return False
         return response
 
 if __name__ == '__main__':
-    url = "http://172.16.68.100:7480"
-    # url = "http://10.255.20.121:7480"
+    # url = "http://172.16.68.100:7480"
+    url = "http://10.255.20.121:7480"
     conn = CONNECTION(url)
     conn.head_object("bucket-1")
